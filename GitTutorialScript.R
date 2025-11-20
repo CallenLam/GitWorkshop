b48cda2a8ab04b3eb405da94c0a845193dae2dc7
CallenLam <callen.lamourie@gmail.com>
2025-11-20 14:12:22 -0500

    add

--- GitTutorialScript.R
+++ GitTutorialScript.R
@@ -1,1 +1,10 @@
 #Val Lucet tutorial
+
+#Commit is equvalaent to a version, taking record of changes since last commit
+#Green highlights is a new change
+#save commit
+
+#now making new changes
+getwd()
+#save file, now git shows M not A, means file was modified
+#hh
